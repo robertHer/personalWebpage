435ef6f3c045e049acb16be3d5b757d13f100903
robertHer <totolero769@gmail.com>
2018-07-14 08:57:30 -0700

    creating intial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HeaderComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent,
+      HeaderComponent,
+      AboutComponent,
+      ContactComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  bannerImg = '../../assets/photos/website-banner.png'
+  
 
   constructor() { }
 
